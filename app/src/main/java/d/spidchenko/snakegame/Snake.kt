44d39b14f11ff513538ae36de23ba9975b8a8ad5
Dmitry Spidchenko <spidchenko.d@gmail.com>
2022-06-24 00:44:37 +0300

    Added new game classes

---
+++ app/src/main/java/d/spidchenko/snakegame/Snake.kt
@@ -0,0 +1,5 @@
+package d.spidchenko.snakegame
+
+class Snake {
+
+}
--- app/src/main/java/d/spidchenko/snakegame/SnakeGame.kt
+++ app/src/main/java/d/spidchenko/snakegame/SnakeGame.kt
@@ -1,9 +1,34 @@
 package d.spidchenko.snakegame
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.media.SoundPool
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class SnakeGame(context: Context) : SurfaceView(context) {
+
+    private lateinit var mGameThread: Thread
+    private var mNextFrameTime = 0L
+    private var mPlaying = false
+    private var mPaused = true
+    private var mSceneInitialized = false
+
+    private val mSurfaceHolder: SurfaceHolder = holder
+    private lateinit var mCanvas: Canvas
+    private val mPaint: Paint = Paint()
+
+    private lateinit var mSoundPool: SoundPool
+    private var mEatId: Int = 0
+    private var mCrashId: Int = 0
+
+    private var mNumBlocksHigh = 0
+    private var mScore = 0
+    private val mSnake = Snake()
+    private val mApple = Apple()
+
+
     fun resume() {
         TODO("Not yet implemented")
     }
@@ -12,4 +37,8 @@ class SnakeGame(context: Context) : SurfaceView(context) {
         TODO("Not yet implemented")
     }
 
+    companion object{
+        private const val TAG = "SnakeGame.LOG_TAG"
+        const val NUM_BLOCKS_WIDE = 10
+    }
 }
